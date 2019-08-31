d9fe553b415bba4692232b76a1e66ec80402f3f3
katarzyna.wilkosz.programista@gmail.com <katarzyna.wilkosz.programista@gmail.com>
2019-08-31 20:00:45 +0200

    fix datetimepicker and back history after logout

--- LabStore/Scripts/LabManager/lm-order-datetimepicker.js
+++ LabStore/Scripts/LabManager/lm-order-datetimepicker.js
@@ -2,7 +2,6 @@
     $('#datetimepicker2').datetimepicker({
         format: 'YYYY-MM-DD',
         locale: 'pl',
-        defaultDate: moment(),
         minDate: new Date(),
         sideBySide: true
     });
